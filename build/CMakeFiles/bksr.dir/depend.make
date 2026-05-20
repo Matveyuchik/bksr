052d3944fb4682292209bc16cc5157e5b4cb1594
Matveyuchik <154120337+Matveyuchik@users.noreply.github.com>
2026-05-20 20:14:10 +0300

    delete: build

--- build/CMakeFiles/bksr.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bksr.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/bksr.dir/src/map.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/bksr.dir/src/map.c.o: \
- /mnt/c/Users/metro/Documents/proj/bksr/src/map.c \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/metro/Documents/proj/bksr/src/map.h
